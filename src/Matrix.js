c4e4dd612ab662aac796b9c4251b6e4611512c95
popkorn96 <mpaloma2014@gmail.com>
2020-11-29 15:51:04 -0600

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Cell from './cell.js'
 
 export default class Matrix extends Component {
   
@@ -19,3 +20,16 @@ export default class Matrix extends Component {
   }
   
 }
+Matrix.defaultProps = {
+  values: (() => {
+    const row = ['#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00','#F00',]
+    return (new Array(10).fill(row))
+  })()
+}
+// Matrix
+// Matrix should use its prop, values, to determine its cell colors. This is a 10 x 10 array (essentially making 10 rows, with 10 values in each row). 
+// Because we are responsible React developers, we want to make sure we have a default grid in case no values prop is passed.
+
+// Make a default values prop for Matrix, which is a 10x10 array filled with the values '#F00' (red). For inspiration, take a look at src/data.js.
+// Once you have made your Cell component, replace the return value in genRow's map to: <Cell value={val} />. 
+// Here we are specifying our Cell component should have a value prop.
